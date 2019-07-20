c206a4a383270479ae09d596144980056efef0ae
InsaneLegacy <wesleyvanneck105@outlook.com>
2019-07-20 13:00:04 +0200

    Update Logging.java

--- src/main/java/com/filiphsandstrom/mineiago/Logging.java
+++ src/main/java/com/filiphsandstrom/mineiago/Logging.java
@@ -36,8 +36,8 @@ public class Logging {
 
         String file_content;
         PrintWriter printer;
-        try {
-            file_content = Files.readString(loggingFile.toPath(), StandardCharsets.UTF_8);
+        
+            try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8))
             printer = new PrintWriter(new FileWriter(loggingFile));
         } catch (Exception e) {
             e.printStackTrace();
